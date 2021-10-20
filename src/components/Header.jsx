f7eac8e4d35821987f84dab8d37b0aa3ffcc8e9a
unknown <chennyliu@qq.com>
2021-10-20 18:21:50 -0400

    Complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <BorderColorIcon />
+        Quick Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
